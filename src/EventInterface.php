649096580ccdf75582183ab9fbb21cf39b419114
Anton Ukhanev <xedin.unknown@gmail.com>
2018-04-01 17:58:25 +0200

    Interfaces converted to Dhii form

--- src/EventInterface.php
+++ src/EventInterface.php
@@ -1,95 +1,14 @@
 <?php
 
-namespace Psr\EventManager;
+namespace Dhii\EventManager;
+
+use Psr\EventManager\EventInterface as BaseEventInterface;
 
 /**
- * Interface for Event.
+ * Represents an application event.
  *
- * @since 0.1
+ * @since [*next-version*]
  */
-interface EventInterface
+interface EventInterface extends BaseEventInterface
 {
-    /**
-     * Get event name.
-     *
-     * @since 0.1
-     *
-     * @return string The name of the event.
-     *                The event name contains mixed-case alphanumeric characters, as well as '_' (underscore) and '.' (period).
-     */
-    public function getName();
-
-    /**
-     * Get target/context from which event was triggered.
-     *
-     * @since 0.1
-     *
-     * @return null|string|object The event context, if any.
-     */
-    public function getTarget();
-
-    /**
-     * Get parameters passed to the event.
-     *
-     * @since 0.1
-     *
-     * @return array The map of event parameter names to values.
-     */
-    public function getParams();
-
-    /**
-     * Get a single parameter by name.
-     *
-     * @since 0.1
-     *
-     * @param string $name The name of the parameter to get.
-     *
-     * @return mixed The value of the parameter.
-     */
-    public function getParam($name);
-
-    /**
-     * Set the event name.
-     *
-     * @since 0.1
-     *
-     * @param string $name The name of the event.
-     */
-    public function setName($name);
-
-    /**
-     * Set the event target.
-     *
-     * @since 0.1
-     *
-     * @param null|string|object $target The context of the event.
-     */
-    public function setTarget($target);
-
-    /**
-     * Set event parameters.
-     *
-     * @since 0.1
-     *
-     * @param array $params The map of parameter names to values.
-     */
-    public function setParams(array $params);
-
-    /**
-     * Indicate whether or not to stop propagating this event.
-     *
-     * @since 0.1
-     *
-     * @param bool $flag If true, no further listeners for this event will be triggered.
-     */
-    public function stopPropagation($flag);
-
-    /**
-     * Has this event indicated event propagation should stop?
-     *
-     * @since 0.1
-     *
-     * @return bool True if propagation has been stopped; false otherwise.
-     */
-    public function isPropagationStopped();
 }
--- src/EventManagerInterface.php
+++ src/EventManagerInterface.php
@@ -1,58 +1,16 @@
 <?php
 
-namespace Psr\EventManager;
+namespace Dhii\EventManager;
+
+use Psr\EventManager\EventManagerInterface as BaseEventManagerInterface;
 
 /**
- * Interface for EventManager.
+ * Represents a manager of events.
  *
- * @since 0.1
+ * Responsible for managing event handlers, and dispatching events.
+ *
+ * @since [*next-version*]
  */
-interface EventManagerInterface
+interface EventManagerInterface extends BaseEventManagerInterface
 {
-    /**
-     * Attach a listener to an event.
-     *
-     * @since 0.1
-     *
-     * @param string   $event    The name of the event to attach the listener to.
-     * @param callable $callback The event listener.
-     * @param int      $priority The priority of the listener.
-     *
-     * @return bool True on success; false on failure.
-     */
-    public function attach($event, $callback, $priority = 0);
-
-    /**
-     * Detach a listener from an event.
-     *
-     * @since 0.1
-     *
-     * @param string   $event    The event to detach the listener from.
-     * @param callable $callback The listener to detach.
-     *
-     * @return bool True on success; false on failure.
-     */
-    public function detach($event, $callback);
-
-    /**
-     * Clear all listeners for a given event.
-     *
-     * @since 0.1
-     *
-     * @param string $event The name of the event to detach the listeners from.
-     */
-    public function clearListeners($event);
-
-    /**
-     * Trigger an event.
-     *
-     * @since 0.1
-     *
-     * @param string|EventInterface $event  The event or the name of an event to trigger.
-     * @param object|string         $target The context, for which the event is being triggered, if any.
-     * @param array|object          $argv   The map of event parameters.
-     *
-     * @return mixed The result of the last event listener.
-     */
-    public function trigger($event, $target = null, $argv = array());
 }
